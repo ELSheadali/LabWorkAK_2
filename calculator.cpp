ee2d9cfa3cd4c68b0de2557f22fec136aad762f1
denys <delmer1083@gmail.com>
2023-10-26 19:59:38 +0300

    my 5-15 lines of code

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,12 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+//Here will be my part of code
+//This was written in the end off all tasks
+//It will be doing just a simple division
+//Made by Denys Makarenko
+int Division::Mul (double a, double b)
+{
+    return a / b + 0.5;
+}
